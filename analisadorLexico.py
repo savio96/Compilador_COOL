b53da23b801ae13fd7d2f0a80fbbd349f3870063
Savio <savio.96@hotmail.com>
2022-04-15 12:12:09 -0300

    string

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -1,67 +1,79 @@
 import tokenCool
 from tokenCool import TokenCool
 
+
 class Analisador(TokenCool):
     def __init__(self, conteudo):
         super().__init__()
         self.conteudo = conteudo
         self.estado = 0
-        self.tokenC=TokenCool()
-
+        self.tokenC = TokenCool()
 
     def proxToken(self):
-        atualChar= self.conteudo[0]
-        palavra=""
+        atualChar = self.conteudo[0]
+        palavra = ""
         estado = 0
-        loop=True
-        pos=0
-        #print(len(self.conteudo))
+        loop = True
+        pos = 0
+        contString=0
+        # print(len(self.conteudo))
 
-        while(loop==True):
-            if estado==0:
+        while (loop == True):
+            if estado == 0:
                 if self.ehChar(atualChar):
-                    estado=1
+                    estado = 1
 
                 elif self.ehNumero(atualChar):
-                    estado=3
+                    estado = 3
 
                 elif self.ehEspaco(atualChar):
-                    estado=0
+                    estado = 0
 
+                elif self.ehString(atualChar):
+                    estado = 5
 
-            if estado==1:
-                if self.ehChar(atualChar) or self.ehNumero(atualChar):
-                    estado=1
+            if estado == 1:
+                if self.ehChar(atualChar) or self.ehNumero(atualChar) or atualChar == "_":
+                    estado = 1
                     palavra += atualChar
-                    #atualChar = self.proxChar(pos)
-                    #pos+=1
+                    # atualChar = self.proxChar(pos)
+                    # pos+=1
 
                 else:
-                    estado=2
+                    estado = 2
                     TokenCool.mostrarToken(self.tokenC, estado, palavra)
-                    estado=0
-                    palavra=""
-            if estado==3:
+                    estado = 0
+                    palavra = ""
+            if estado == 3:
                 if self.ehNumero(atualChar):
                     estado = 3
                     palavra += atualChar
-                elif not self.ehNumero(atualChar) and estado==3 and not self.ehEspaco(atualChar):
+                elif not self.ehNumero(atualChar) and estado == 3 and not self.ehEspaco(atualChar):
                     raise Exception('Numero Desconhecido')
                 else:
-                    estado=4
+                    estado = 4
+                    TokenCool.mostrarToken(self.tokenC, estado, palavra)
+                    estado = 0
+                    palavra = ""
+            if estado == 5:
+
+                palavra+=atualChar
+
+                if atualChar=='"' and contString==1:
+                    estado=6
                     TokenCool.mostrarToken(self.tokenC, estado, palavra)
                     estado=0
-                    palavra = ""
+                    palavra=""
+                    contString=0
+                else:
+                    contString = 1
             if self.ehEOF(pos):
                 return None
             else:
                 atualChar = self.proxChar(pos)
                 pos += 1
 
-
-            #print(palavra+"\n")
-
-
+            # print(palavra+"\n")
 
     def ehNumero(self, char):
         if "0" <= char <= "9":
@@ -79,15 +91,20 @@ class Analisador(TokenCool):
         return False
 
     def ehEspaco(self, char):
-        if char == " " or char == "\t" or char == "\n" or char == "\r":
+        if char == " " or char == "\t" or char == "\n" or char == "\r" or char == "\f":
             return True
         return False
 
-    def proxChar(self,pos):
-        return self.conteudo[pos+1]
+    def ehString(self, char):
+        if char == '"':
+            return True
+        return False
 
-    def voltar(self,pos):
-        return self.conteudo[pos-1]
+    def proxChar(self, pos):
+        return self.conteudo[pos + 1]
 
-    def ehEOF(self,pos):
-        return pos == (len(self.conteudo))-1
+    def voltar(self, pos):
+        return self.conteudo[pos - 1]
+
+    def ehEOF(self, pos):
+        return pos == (len(self.conteudo)) - 1
--- tokenCool.py
+++ tokenCool.py
@@ -4,9 +4,10 @@ class TokenCool():
     def __init__(self):
         self.TK_IDENTIFICADOR=0
         self.TK_NUM=1
-        self.TK_OPERADOR=2
-        self.TK_PONTUACAO=3
-        self.TK_ATRIBUICAO=4
+        self.TK_STRING=2
+        self.TK_OPERADOR=3
+        self.TK_PONTUACAO=4
+        self.TK_ATRIBUICAO=5
         self.tipo = -1
         self.texto=""
 
@@ -26,5 +27,7 @@ class TokenCool():
     def mostrarToken(self,estado, texto):
         if estado==2:
             print("Token: "+texto + ": ",self.TK_IDENTIFICADOR)
-        if estado==4:
+        if estado==4 :
             print("Token: " + texto + ": ", self.TK_NUM)
+        if estado==6:
+            print("Token: " + texto + ": ", self.TK_STRING)
